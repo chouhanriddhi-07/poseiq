efde9e9b7bf52800a19d0831f1154f304dd89af5
Riddhi Chouhan <chouhan.riddhi@gmail.com>
2026-05-13 18:46:01 -0400

    Scaffolded PoseIQ

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to PoseIQ!</h1>
+      <p>This is a simple React application using TypeScript and MediaPipe.</p>
+    </div>
+  )
+}
+
+export default App
